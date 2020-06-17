17fc30231d6129ae00938988ad4c5b2c8a1e00e9
PhMueller <muller-phil@gmx.net>
2020-06-17 13:31:13 +0200

    Add Example for usage

---
+++ examples/cartpole_bohb.py
@@ -0,0 +1,36 @@
+"""
+BOHB on Cartpole
+================
+
+This example shows the usage of an Hyperparameter Tuner, such as BOHB on the cartpole benchmark.
+BOHB is a combination of Bayesian optimization and Hyperband.
+
+Please install the necessary dependencies via ``pip install .[cartpole_example]``
+and the HPOlib3 ``pip install <dir of hpolib>``
+"""
+
+import logging
+from pathlib import Path
+
+from HPOlibExperimentUtils.core.optimizer import OptimizerEnum
+from HPOlibExperimentUtils.run_benchmark import run_benchmark
+from HPOlibExperimentUtils.validate_benchmark import validate_benchmark
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('BOHB on cartpole')
+root_logger = logging.getLogger()
+
+root_logger.setLevel(logging.DEBUG)
+
+benchmark = 'cartpolereduced'
+output_dir = Path('../example_dir/cartpole_bohb2')
+rng = 1
+
+run_benchmark(optimizer=OptimizerEnum.BOHB,
+              benchmark=benchmark,
+              output_dir=output_dir,
+              rng=rng)
+
+validate_benchmark(benchmark=benchmark,
+                   output_dir=output_dir,
+                   rng=rng)
