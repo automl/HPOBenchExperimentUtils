61714252f25b89ab3f495f74b0b34d1e888f754e
Archit Bansal <archit.bansal.93@gmail.com>
2020-08-04 17:43:22 +0200

    Added optimizer history, some readability improvements.

--- HPOlibExperimentUtils/optimizer/dragonfly_optimizer.py
+++ HPOlibExperimentUtils/optimizer/dragonfly_optimizer.py
@@ -42,9 +42,8 @@ class DragonflyOptimizer(Optimizer):
                       if key in known_fidelities}
         logger.debug(f"Using fidelities: %s" % fidelities)
         config, domain_parsers, fidelity_parsers = configspace_to_dragonfly(domain_cs=self.cs, fidely_cs=fidelities)
-        fidelity_costs = [tup[2] for tup in fidelity_parsers] # Separate the costs
+        fidelity_costs = [tup[2] for tup in fidelity_parsers]  # Separate the costs
         fidelity_parsers = [(tup[0], tup[1]) for tup in fidelity_parsers]
-        # config, domain_parsers = configspace_to_dragonfly(domain_cs=self.cs, fidely_cs=None)
 
         self.optimizer_settings["max_or_min"] = "min"
         options, config = load_dragonfly_options(options=self.optimizer_settings, config=config)
@@ -70,15 +69,23 @@ class DragonflyOptimizer(Optimizer):
 
         if is_mf:
             def parse_fidelities(z):
-                return {parser[0]: parser[1](val) for parser, val in zip(fidelity_parsers, z)}
+                ret = {parser[0]: parser[1](val) for parser, val in zip(fidelity_parsers, z)}
+                return ret
 
             def objective_mf(z, x):
-                return self.benchmark.objective_function(parse_domain(x), **parse_fidelities(z))['function_value']
+                conf = parse_domain(x)
+                fidels = parse_fidelities(z)
+                logger.debug("Calling multi-fidelity objective with configuration %s at fidelity %s" % (
+                    conf.get_dictionary(), fidels))
+                ret = self.benchmark.objective_function(conf, **fidels)
+                logger.debug("multi-fidelity objective returned %s" % ret)
+                return ret['function_value']
 
             def cost(z):
-                return sum([c(v) for c, v in zip(fidelity_costs, z)])
+                ret = sum([c(v) for c, v in zip(fidelity_costs, z)]) / float(len(fidelity_costs)) + 1e-6
+                return ret
 
-            logger.debug('Minimising multi-fidelity function on\n Fidelity-Space: %s.\n Domain: %s.\n' % (
+            logger.info('Minimising multi-fidelity function on\n Fidelity-Space: %s.\n Domain: %s.' % (
                 config.fidel_space, config.domain))
             opt_val, opt_pt, history = minimise_multifidelity_function(
                 objective_mf, fidel_space=None, domain=None,
@@ -87,14 +94,16 @@ class DragonflyOptimizer(Optimizer):
                 opt_method=options.opt_method, config=config, options=options,
                 reporter=options.report_progress)
         else:
-            print('Minimising function on Domain: %s.\n' % config.domain)
+            # test_obj = objective(test_point)
+            logger.info('Minimising function on Domain: %s.' % config.domain)
             opt_val, opt_pt, history = minimise_function(
                 objective, domain=None, max_capital=options.max_capital,
                 capital_type=options.capital_type, opt_method=options.opt_method,
                 config=config, options=options, reporter=options.report_progress)
 
         generate_trajectory(history, save_file=self.optimizer_settings["output_dir"] / Constants.smac_traj_filename,
-                            is_cp=True if isinstance(history.query_qinfos[0].point, list) else False)
+                            is_cp=True if isinstance(history.query_qinfos[0].point, list) else False,
+                            history_file=self.optimizer_settings["output_dir"] / "saved_history.json")
 
         # Ok following
         # https://stackoverflow.com/questions/2837214/python-popen-command-wait-until-the-command-is-finished
