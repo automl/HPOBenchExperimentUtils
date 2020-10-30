bd254bf4e6c6fab53acbdb72221ed00dc9aa2a9a
eggenspk <eggenspk@informatik.uni-freiburg.de>
2020-10-30 10:41:42 +0100

    ADD options for plotting

--- HPOlibExperimentUtils/evaluate_benchmark.py
+++ HPOlibExperimentUtils/evaluate_benchmark.py
@@ -20,8 +20,39 @@ logging.basicConfig(level=logging.INFO, format=_default_log_format)
 
 set_env_variables_to_use_only_one_core()
 
+plot_dc = {
+    "BNNOnBostonHousing": {
+    # BOHB paper
+    "xlim_lo": 10**3,
+    "ylim_lo": 3,
+    "ylim_up": 9,
+    "xscale": "log",
+    "yscale": "linear",
+    },
+    "BNNOnProteinStructure": {
+    "xlim_lo": 10**3,
+    "ylim_lo": 2,
+    "ylim_up": 10,
+    "xscale": "log",
+    "yscale": "linear",
+    },
+    "BNNOnYearPrediction": {
+    "xlim_lo": 10**3,
+    "ylim_lo": 10,
+    "ylim_up": 50,
+    "xscale": "log",
+    "yscale": "linear",
+    },
+    "cartpolereduces": {
+    "xlim_lo": 10**1,
+    "ylim_lo": 10**2,
+    "ylim_up": 10**4,
+    "xscale": "log",
+    "yscale": "log",
+    },
+}
 
-def save_table(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str], rng: int,
+def save_table(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str],
                unvalidated: bool = True, **kwargs):
     _log.info(f'Start creating table of benchmark {benchmark}')
     input_dir = Path(input_dir) / benchmark
@@ -61,8 +92,8 @@ def save_table(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Pa
         fh.write(result_df.to_latex())
 
 
-def plot_trajectory(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str], rng: int,
-                    criterion: str = 'mean', unvalidated: bool = True):
+def plot_trajectory(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str],
+                    criterion: str = 'mean', unvalidated: bool = True, **kwargs):
 
     _log.info(f'Start plotting trajectories of benchmark {benchmark}')
     input_dir = Path(input_dir) / benchmark
@@ -79,26 +110,37 @@ def plot_trajectory(benchmark: str, output_dir: Union[Path, str], input_dir: Uni
 
     # start plotting the trajectories:
     f, ax = plt.subplots(1, 1)
-
+    min_ = 100000
+    max_ = -1
     for key, df in zip(keys, statistics_df):
 
         df[criterion].plot.line(drawstyle='steps-post', linewidth=2, ax=ax, label=key)
+        min_ = min(min_, df[criterion].min())
+        max_ = max(max_, df[criterion].max())
         if criterion == 'mean':
             ax.fill_between(df.index, df['mean'] - df['std'], df['mean'] + df['std'], alpha=0.3)
         else:
             ax.fill_between(df.index, df['q25'], df['q75'], alpha=0.3)
 
     ax.set_ylabel('%s Loss' % criterion)
-    xl = ax.get_xlim()
-    ax.set_xlim([1, xl[1]])
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_title(f'{benchmark} {val_str} {criterion} Seed {rng}')
+    xl, xu = ax.get_xlim()
+    
+    benchmark_spec = plot_dc.get(benchmark, {})
+    xl = benchmark_spec.get("xlim_lo", xl)
+    xu = benchmark_spec.get("xlim_up", xu)
+    yl = benchmark_spec.get("ylim_lo", min_)
+    yu = benchmark_spec.get("ylim_up", max_)
+    xscale = benchmark_spec.get("xscale", "log")
+    yscale = benchmark_spec.get("yscale", "log")
+
+    ax.set_xlim([xl, xu])
+    ax.set_ylim([yl, yu])
+    ax.set_xscale(xscale)
+    ax.set_yscale(yscale)
+    ax.set_title(f'{benchmark} {val_str} {criterion}')
     ax.legend()
     val_str = 'unvalidated' if unvalidated else 'validated'
     plt.savefig(Path(output_dir) / f'{benchmark}_{val_str}_{criterion}_trajectory.png')
-    #print(statistics_df)
-
     return 1
 
 
@@ -109,10 +151,14 @@ if __name__ == "__main__":
     parser.add_argument('--output_dir', required=True, type=str)
     parser.add_argument('--input_dir', required=True, type=str)
     parser.add_argument('--benchmark', choices=get_benchmark_names(), required=True, type=str)
-    parser.add_argument('--rng', required=False, default=0, type=int)
+    parser.add_argument('--what', choices=["all", "table", "over_time"], default="all")
+    parser.add_argument('--agg', choices=["mean", "median"], default="median")
     parser.add_argument('--unvalidated', action='store_true', default=False)
 
     args, unknown = parser.parse_known_args()
-    save_table(**vars(args))
-    #plot_trajectory(criterion='mean', **vars(args))
-    plot_trajectory(criterion='median', **vars(args))
+
+    if args.what in ("all", "table"):
+        save_table(**vars(args))
+
+    if args.what in ("all", "over_time"):
+        plot_trajectory(criterion=args.agg, **vars(args))
