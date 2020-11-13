60eea42591fad2d40884f8a888b54006bd2ce647
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2020-11-13 17:43:04 +0100

    Clean settings (#8)
    * RM setting we don't need
    
    * FIX benchmark returnin NaN; replace w/ MAXINT
    
    * UPDATE cmd file creation
    
    * ADD options for plotting
    
    * RM setting we don't need
    
    * FIX benchmark returnin NaN; replace w/ MAXINT
    
    * UPDATE cmd file creation
    
    * ADD options for plotting
    
    * Reorganize analysis scripts
    
    * ADD new plots
    
    * UPDATE cmd generation script
    
    * ADD init
    
    * FIX start and finish_time
    
    * fix typo
    
    * FIX plot wrt to time
    
    * FIX fidelity plot
    
    * FIX timelimits
    
    * ADD script to load all containers;FIX create_cmd
    
    * FIX too many val cmds
    
    * ADD plotting regret
    
    * FIX NASTAB timelimits
    
    * FIX ylabel for plots
    
    * FIX limits and add grid
    
    * FIX limits
    
    * ADD individual validation cmds
    
    * FIX limits

---
+++ HPOlibExperimentUtils/analysis/stats_generation.py
@@ -0,0 +1,110 @@
+import logging
+from pathlib import Path
+from typing import Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from HPOlibExperimentUtils import _default_log_format, _log as _main_log
+from HPOlibExperimentUtils.utils.validation_utils import load_trajectories, \
+    load_trajectories_as_df, df_per_optimizer
+from HPOlibExperimentUtils.utils.plotting_utils import color_per_opt, marker_per_opt
+from HPOlibExperimentUtils.utils.runner_utils import get_optimizer_setting
+
+_main_log.setLevel(logging.DEBUG)
+_log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format=_default_log_format)
+
+
+def plot_fidels(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str], **kwargs):
+    _log.info(f'Plotting evaluated fidelities of benchmark {benchmark}')
+    input_dir = Path(input_dir) / benchmark
+    assert input_dir.is_dir(), f'Result folder doesn\"t exist: {input_dir}'
+    opt_rh_dc = load_trajectories_as_df(input_dir=input_dir,
+                                        which="runhistory")
+
+    stat_dc = {}
+    for opt in opt_rh_dc:
+        if len(opt_rh_dc) == 0: continue
+        rhs = load_trajectories(opt_rh_dc[opt])
+        df = df_per_optimizer(opt, rhs)
+        stat_dc[opt] = df
+
+    plt.figure(figsize=[5*len(stat_dc), 5])
+
+    ax_old = None
+    for i, opt in enumerate(stat_dc):
+        ax = plt.subplot(1, len(stat_dc), i+1, sharey=ax_old)
+        # get queried fidels
+        df = stat_dc[opt]
+        nseeds = df['id'].unique()
+        avg = []
+        for seed in nseeds:
+            fidels = df[df['id'] == seed]["fidel_values"]
+            avg.append(len(fidels))
+            steps = df[df['id'] == seed]["total_time_used"]
+            label = get_optimizer_setting(opt).get("display_name", opt)
+
+            plt.scatter(steps, fidels, edgecolor=color_per_opt.get(opt, "k"), facecolor="none",
+                        marker=marker_per_opt[opt], alpha=0.5,
+                        label=label if seed == nseeds[-1] else None)
+        plt.xscale("log")
+        plt.xlabel("Runtime in seconds")
+        plt.legend(title="%g evals on avg" % np.mean(avg))
+        ax_old = ax
+
+    plt.ylabel("Fidelity")
+    plt.tight_layout()
+    plt.savefig(Path(output_dir) / f'{benchmark}_fidel.png')
+
+
+def plot_overhead(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str], **kwargs):
+    _log.info(f'Start plotting overhead of benchmark {benchmark}')
+    input_dir = Path(input_dir) / benchmark
+    assert input_dir.is_dir(), f'Result folder doesn\"t exist: {input_dir}'
+    opt_rh_dc = load_trajectories_as_df(input_dir=input_dir,
+                                        which="runhistory")
+
+    stat_dc = {}
+    for opt in opt_rh_dc:
+        if len(opt_rh_dc) == 0: continue
+        rhs = load_trajectories(opt_rh_dc[opt])
+        df = df_per_optimizer(opt, rhs)
+        stat_dc[opt] = df
+
+    plt.figure(figsize=[5, 5])
+    a = plt.subplot(111)
+    for opt in stat_dc:
+        # get queried fidels
+        df = stat_dc[opt]
+        nseeds = df['id'].unique()
+        for seed in nseeds:
+            steps = df[df['id'] == seed]["total_time_used"]
+            label = get_optimizer_setting(opt).get("display_name", opt)
+
+            benchmark_cost = df[df['id'] == seed]["finish_time"] - df[df['id'] == seed]["start_time"]
+            benchmark_cost = np.cumsum(benchmark_cost)
+            plt.plot(steps, benchmark_cost, color='k', alpha=0.5, zorder=99,
+                     label=benchmark if seed == 0 and opt == list(stat_dc.keys())[0] else None)
+
+            overhead = df[df['id'] == seed]["start_time"] - df[df['id'] == seed]["finish_time"].shift(1)
+            overhead = np.cumsum(overhead)
+            plt.plot(steps, overhead, color=color_per_opt.get(opt, "k"), linestyle=":", label=label if seed == 0 else None)
+
+            overall_cost = df[df['id'] == seed]["finish_time"] - df[df['id'] == seed]["start_time"].iloc[0]
+            benchmark_cost = np.cumsum(overall_cost)
+            plt.plot(steps, overall_cost, color=color_per_opt.get(opt, "k"), alpha=0.5, zorder=99,
+                     label="%s overall" % label if seed == 0 else None)
+
+
+    a.legend()
+    a.grid(which="both", zorder=0)
+    a.set_yscale("log")
+    a.set_xscale("log")
+    a.set_xlabel("Runtime in seconds")
+    a.set_ylabel("Cumulated overhead in seconds")
+    a.set_xlim([1, a.set_xlim()[1]])
+    #a.set_ylim([0.1, 10000])
+    plt.tight_layout()
+    plt.savefig(Path(output_dir) / f'{benchmark}_overhead.png')
+
---
+++ scripts/load_all_containers.py
@@ -0,0 +1,20 @@
+from importlib import import_module
+import yaml
+import os
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+settings_file = "%s/HPOlibExperimentUtils/" % base_dir
+experiment_settings_path = "%s/HPOlibExperimentUtils/benchmark_settings.yaml" % settings_file
+with open(experiment_settings_path, 'r') as fh:
+    experiment_settings = yaml.load(fh, yaml.FullLoader)
+
+for key in experiment_settings:
+    import_str = 'hpolib.' + 'container.' + 'benchmarks.' + experiment_settings[key]["import_from"]
+    module = import_module(import_str)
+    benchmark_obj = getattr(module, experiment_settings[key]["import_benchmark"])
+    if key in ("xgboost", "svm"):
+        benchmark_obj = benchmark_obj(task_id=167149)
+    else:
+        benchmark_obj = benchmark_obj()
+
+    del benchmark_obj
