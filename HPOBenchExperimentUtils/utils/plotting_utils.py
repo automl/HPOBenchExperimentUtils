5156598efc7ea86030e6c440146e415c63d0c67f
eggenspk <eggenspk@informatik.uni-freiburg.de>
2020-11-27 21:20:10 +0100

    FIX incumbent vals

--- HPOBenchExperimentUtils/utils/plotting_utils.py
+++ HPOBenchExperimentUtils/utils/plotting_utils.py
@@ -62,7 +62,7 @@ plot_dc = {
         # best_test = np.min(te)
         # best_valid = np.min(ve)
         "ystar_valid": 0.00019159916337230243, #0.00020406871,
-        "ystar_test": 0.00014428208305616863,
+        "ystar_test": 0.0001442820794181898,
     },
     "ProteinStructureBenchmark": {
         "xlim_lo": 10**0,
@@ -77,8 +77,8 @@ plot_dc = {
         # configs, te, ve = [], [], []
         # for k in bench.benchmark.data.keys():
         #     t = bench.benchmark.data[k]["final_test_error"]
-        #     t = [float(i) for i in t]
-        #     te.append(np.sum(t) / len(t))
+        #     # do this to be consistent w/ HPOBench and tabular_nas code
+        #     te.append(float(np.mean(t)))
         #     v = bench.benchmark.data[k]["valid_mse"][:, -1]
         #     v = [float(i) for i in v]
         #     ve.append(np.sum(v) / len(v))
@@ -86,7 +86,7 @@ plot_dc = {
         # best_test = np.min(te)
         # best_valid = np.min(ve)
         "ystar_valid": 0.221378855407238, #0.22137885,
-        "ystar_test": 0.21536805480718613,
+        "ystar_test": 0.21536806225776672,
     },
     "NavalPropulsionBenchmark": {
         "xlim_lo": 10**0,
@@ -101,8 +101,8 @@ plot_dc = {
         # configs, te, ve = [], [], []
         # for k in bench.benchmark.data.keys():
         #     t = bench.benchmark.data[k]["final_test_error"]
-        #     t = [float(i) for i in t]
-        #     te.append(np.sum(t) / len(t))
+        #     # do this to be consistent w/ HPOBench and tabular_nas code
+        #     te.append(float(np.mean(t)))
         #     v = bench.benchmark.data[k]["valid_mse"][:, -1]
         #     v = [float(i) for i in v]
         #     ve.append(np.sum(v) / len(v))
@@ -110,7 +110,7 @@ plot_dc = {
         # best_test = np.min(te)
         # best_valid = np.min(ve)
         "ystar_valid": 3.19113473778998e-05, #3.1911346e-05,
-        "ystar_test": 2.9110290597600397e-05,
+        "ystar_test": 2.91102915070951e-05
     },
     "ParkinsonsTelemonitoringBenchmark": {
         "xlim_lo": 10**0,
@@ -125,8 +125,8 @@ plot_dc = {
         # configs, te, ve = [], [], []
         # for k in bench.benchmark.data.keys():
         #     t = bench.benchmark.data[k]["final_test_error"]
-        #     t = [float(i) for i in t]
-        #     te.append(np.sum(t) / len(t))
+        #     # do this to be consistent w/ HPOBench and tabular_nas code
+        #     te.append(float(np.mean(t)))
         #     v = bench.benchmark.data[k]["valid_mse"][:, -1]
         #     v = [float(i) for i in v]
         #     ve.append(np.sum(v) / len(v))
@@ -134,7 +134,7 @@ plot_dc = {
         # best_test = np.min(te)
         # best_valid = np.min(ve)
         "ystar_valid": 0.0067059280117973685, #0.007629349,
-        "ystar_test": 0.004239296889863908,
+        "ystar_test": 0.00423929700627923,
     },
     "NASCifar10ABenchmark": {
         "xlim_lo": 10**1,
@@ -172,12 +172,25 @@ plot_dc = {
         "ylim_up": 10**2,
         "xscale": "log",
         "yscale": "log",
-        #  cifar100           train       (9930, 99.93733333333334)
-        # cifar100            x-valid     (9930, 73.4933333577474)    obj_func
-        # cifar100            x-test      (9930, 73.51333332112631)
-        # cifar100            ori-test    (9930, 73.50333333333333)   obj_func_test
-        "ystar_valid": 26.5066666422526,
-        "ystar_test": 26.49666666666667,
+        # from hpobench.benchmarks.nas.nasbench_201 import Cifar100NasBench201Benchmark
+        # a = Cifar100NasBench201Benchmark()
+        # keys = a.data[(777, 'train_acc1es')].keys()
+        #
+        # configs, te, ve = [], [], []
+        # for k in keys:
+        #     t1 = a.data[(777, "valid_acc1es")][k][199]
+        #     t2 = a.data[(888, "valid_acc1es")][k][199]
+        #     t3 = a.data[(999, "valid_acc1es")][k][199]
+        #     te.append(float(100 - np.mean([t1, t2, t3])))
+        #     v1 = a.data[(777, "test_acc1es")][k]
+        #     v2 = a.data[(888, "test_acc1es")][k]
+        #     v3 = a.data[(999, "test_acc1es")][k]
+        #     ve.append(float(100 - np.mean([v1, v2, v3])))
+        # best_test = np.min(te)
+        # best_valid = np.min(ve)
+        # print(b, best_test, best_valid)
+        "ystar_valid": 26.49666666666667,
+        "ystar_test": 26.48666667887369,
    },
    "Cifar10ValidNasBench201Benchmark": {
         "xlim_lo": 10**2,
@@ -185,12 +198,25 @@ plot_dc = {
         "ylim_up": 10**2,
         "xscale": "log",
         "yscale": "log",
-        # cifar10-valid       train       (10154, 100.0)
-        # cifar10-valid       x-valid     (6111, 91.60666665039064)   obj_func
-        # cifar10-valid       ori-test    (1459, 91.52333333333333)   obj_func_test
-        # [27.11.2020] Note: Adding one more digit for ystar_valid
+        # from hpobench.benchmarks.nas.nasbench_201 import Cifar10ValidNasBench201Benchmark
+        # a = Cifar10ValidNasBench201Benchmark()
+        # keys = a.data[(777, 'train_acc1es')].keys()
+        #
+        # configs, te, ve = [], [], []
+        # for k in keys:
+        #     t1 = a.data[(777, "valid_acc1es")][k][199]
+        #     t2 = a.data[(888, "valid_acc1es")][k][199]
+        #     t3 = a.data[(999, "valid_acc1es")][k][199]
+        #     te.append(float(100 - np.mean([t1, t2, t3])))
+        #     v1 = a.data[(777, "test_acc1es")][k]
+        #     v2 = a.data[(888, "test_acc1es")][k]
+        #     v3 = a.data[(999, "test_acc1es")][k]
+        #     ve.append(float(100 - np.mean([v1, v2, v3])))
+        # best_test = np.min(te)
+        # best_valid = np.min(ve)
+        # print(b, best_test, best_valid)
         "ystar_valid": 8.393333349609364,
-        "ystar_test": 8.47666666666667,
+        "ystar_test": 8.476666666666674,
    },
    "Cifar10NasBench201Benchmark":  {
         "xlim_lo": 10**2,
@@ -198,8 +224,25 @@ plot_dc = {
         "ylim_up": 20,
         "xscale": "log",
         "yscale": "log",
-        # cifar10             train       (10484, 99.994)
-        # cifar10             ori-test    (6111, 94.37333333333333)   both
+        # from hpobench.benchmarks.nas.nasbench_201 import ImageNetNasBench201Benchmark
+        # a = ImageNetNasBench201Benchmark()
+        # keys = a.data[(777, 'train_acc1es')].keys()
+        #
+        # configs, te, ve = [], [], []
+        # for k in keys:
+        #     t1 = a.data[(777, "valid_acc1es")][k][199]
+        #     t2 = a.data[(888, "valid_acc1es")][k][199]
+        #     t3 = a.data[(999, "valid_acc1es")][k][199]
+        #     te.append(float(100 - np.mean([t1, t2, t3])))
+        #     v1 = a.data[(777, "test_acc1es")][k]
+        #     v2 = a.data[(888, "test_acc1es")][k]
+        #     v3 = a.data[(999, "test_acc1es")][k]
+        #     ve.append(float(100 - np.mean([v1, v2, v3])))
+        # best_test = np.min(te)
+        # best_valid = np.min(ve)
+        # print(b, best_test, best_valid)
+        "ystar_valid": 5.626666666666665,
+        "ystar_test": 5.626666666666665,
    },
    "ImageNetNasBench201Benchmark":   {
         "xlim_lo": 10**2,
@@ -207,12 +250,25 @@ plot_dc = {
         "ylim_up": 10**2,
         "xscale": "log",
         "yscale": "log",
-        # ImageNet16-120      train       (9930, 73.22918040138735)
-        # ImageNet16-120      x-valid     (10676, 46.73333327229818)  obj_func
-        # ImageNet16-120      x-test      (857, 47.31111100599501)
-        # ImageNet16-120      ori-test    (857, 46.8444444647895)     obj_func_test
-        "ystar_valid": 53.26666672770182,
-        "ystar_test": 53.1555555352105,
+        # from hpobench.benchmarks.nas.nasbench_201 import ImageNetNasBench201Benchmark
+        # a = ImageNetNasBench201Benchmark()
+        # keys = a.data[(777, 'train_acc1es')].keys()
+        #
+        # configs, te, ve = [], [], []
+        # for k in keys:
+        #     t1 = a.data[(777, "valid_acc1es")][k][199]
+        #     t2 = a.data[(888, "valid_acc1es")][k][199]
+        #     t3 = a.data[(999, "valid_acc1es")][k][199]
+        #     te.append(float(100 - np.mean([t1, t2, t3])))
+        #     v1 = a.data[(777, "test_acc1es")][k]
+        #     v2 = a.data[(888, "test_acc1es")][k]
+        #     v3 = a.data[(999, "test_acc1es")][k]
+        #     ve.append(float(100 - np.mean([v1, v2, v3])))
+        # best_test = np.min(te)
+        # best_valid = np.min(ve)
+        # print(b, best_test, best_valid)
+        "ystar_valid": 53.1555555352105,
+        "ystar_test": 52.68888899400499,
    },
 }
 
