0a525e6d63594bf870292a9a2918a79c92e0cdb9
eggenspk <eggenspk@informatik.uni-freiburg.de>
2020-12-16 14:53:04 +0100

    ADD ecdf plots again

--- HPOBenchExperimentUtils/analysis/stats_generation.py
+++ HPOBenchExperimentUtils/analysis/stats_generation.py
@@ -8,7 +8,7 @@ import numpy as np
 from HPOBenchExperimentUtils import _default_log_format, _log as _main_log
 from HPOBenchExperimentUtils.utils.validation_utils import load_json_files, \
     load_trajectories_as_df, df_per_optimizer
-from HPOBenchExperimentUtils.utils.plotting_utils import color_per_opt, marker_per_opt
+from HPOBenchExperimentUtils.utils.plotting_utils import plot_dc, color_per_opt, marker_per_opt
 from HPOBenchExperimentUtils.utils.runner_utils import get_optimizer_setting
 
 _main_log.setLevel(logging.DEBUG)
@@ -117,3 +117,54 @@ def plot_overhead(benchmark: str, output_dir: Union[Path, str], input_dir: Union
     plt.tight_layout()
     plt.savefig(Path(output_dir) / f'{benchmark}_overhead.png')
     plt.close('all')
+
+
+def plot_ecdf(benchmark: str, output_dir: Union[Path, str], input_dir: Union[Path, str], **kwargs):
+    _log.info(f'Start plotting ECDFs for benchmark {benchmark}')
+    input_dir = Path(input_dir) / benchmark
+    assert input_dir.is_dir(), f'Result folder doesn\"t exist: {input_dir}'
+    opt_rh_dc = load_trajectories_as_df(input_dir=input_dir,
+                                        which="runhistory")
+    benchmark_spec = plot_dc.get(benchmark, {})
+    y_best = benchmark_spec.get("ystar_valid", 0)
+
+    def ecdf(x):
+        xs = np.sort(x)
+        ys = np.arange(1, len(xs) + 1) / float(len(xs))
+        return xs, ys
+
+    stat_dc = {}
+    for opt in opt_rh_dc:
+        if len(opt_rh_dc) == 0: continue
+        rhs = load_json_files(opt_rh_dc[opt])
+        df = df_per_optimizer(opt, rhs, y_best=y_best)
+        stat_dc[opt] = df
+
+    plt.figure(figsize=[5, 5])
+    a = plt.subplot(111)
+    for opt in stat_dc:
+        color = color_per_opt.get(opt, "k")
+        df = stat_dc[opt]
+
+        obj_vals = df["function_values"]
+        x, y = ecdf(obj_vals.to_numpy())
+        label = get_optimizer_setting(opt).get("display_name", opt)
+        plt.plot(x, y, c=color, linewidth=2, label=label)
+
+        nseeds = df['id'].unique()
+        for seed in nseeds:
+            obj_vals = df[df['id'] == seed]["function_values"]
+            x, y = ecdf(obj_vals.to_numpy())
+            plt.plot(x, y, c=color, alpha=0.2)
+
+    if y_best != 0:
+        plt.xlabel("Optimization Regret")
+    else:
+        plt.xlabel("Optimization objective value")
+    plt.ylabel("P(x < X)")
+    yscale = benchmark_spec.get("yscale", "log")
+    plt.xscale(yscale)
+    plt.tight_layout()
+    plt.legend()
+    plt.grid(b=True, which="both", axis="both", alpha=0.5)
+    plt.savefig(Path(output_dir) / f'{benchmark}_ecdf.png')
