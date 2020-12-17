b374f325b3b4c1850ecc4fb09918224dfcf20987
eggenspk <eggenspk@informatik.uni-freiburg.de>
2020-12-17 11:22:39 +0100

    ADD paramnet benchmarks

--- HPOBenchExperimentUtils/utils/plotting_utils.py
+++ HPOBenchExperimentUtils/utils/plotting_utils.py
@@ -218,32 +218,6 @@ plot_dc = {
         "ystar_valid": 8.393333349609364,
         "ystar_test": 8.476666666666674,
    },
-   "Cifar10NasBench201Benchmark":  {
-        "xlim_lo": 10**2,
-        "ylim_lo": 5,
-        "ylim_up": 20,
-        "xscale": "log",
-        "yscale": "log",
-        # from hpobench.benchmarks.nas.nasbench_201 import ImageNetNasBench201Benchmark
-        # a = ImageNetNasBench201Benchmark()
-        # keys = a.data[(777, 'train_acc1es')].keys()
-        #
-        # configs, te, ve = [], [], []
-        # for k in keys:
-        #     t1 = a.data[(777, "valid_acc1es")][k][199]
-        #     t2 = a.data[(888, "valid_acc1es")][k][199]
-        #     t3 = a.data[(999, "valid_acc1es")][k][199]
-        #     te.append(float(100 - np.mean([t1, t2, t3])))
-        #     v1 = a.data[(777, "test_acc1es")][k]
-        #     v2 = a.data[(888, "test_acc1es")][k]
-        #     v3 = a.data[(999, "test_acc1es")][k]
-        #     ve.append(float(100 - np.mean([v1, v2, v3])))
-        # best_test = np.min(te)
-        # best_valid = np.min(ve)
-        # print(b, best_test, best_valid)
-        "ystar_valid": 5.626666666666665,
-        "ystar_test": 5.626666666666665,
-   },
    "ImageNetNasBench201Benchmark":   {
         "xlim_lo": 10**2,
         "ylim_lo": 10**-5,
@@ -270,6 +244,141 @@ plot_dc = {
         "ystar_valid": 53.1555555352105,
         "ystar_test": 52.68888899400499,
    },
+   "NASBench1shot1SearchSpace1Benchmark": {
+        "xlim_lo": 10**2,
+        "ylim_lo": 10**-5,
+        "ylim_up": 10**2,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "NASBench1shot1SearchSpace2Benchmark": {
+        "xlim_lo": 10**2,
+        "ylim_lo": 10**-5,
+        "ylim_up": 10**2,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "NASBench1shot1SearchSpace3Benchmark": {
+        "xlim_lo": 10**2,
+        "ylim_lo": 10**-5,
+        "ylim_up": 10**2,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetAdultOnStepsBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3,
+        "ylim_up": 10**1,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetAdultOnTimeBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3,
+        "ylim_up": 10**1,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetHiggsOnStepsBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3,
+        "ylim_up": 10**-1,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetHiggsOnTimeBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3,
+        "ylim_up": 10**-1,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetLetterOnStepsBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3.5,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetLetterOnTimeBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3.5,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetMnistOnStepsBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-4.5,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetMnistOnTimeBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-4.5,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetOptdigitsOnStepsBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3.5,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetOptdigitsOnTimeBenchmark": {
+        "xlim_lo": 10**0,
+        "ylim_lo": 10**-3.5,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetPokerOnStepsBenchmark": {
+        "xlim_lo": 10**1,
+        "ylim_lo": 10**-4,
+        "ylim_up": 10**0,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   },
+   "ParamNetPokerOnTimeBenchmark": {
+        "xlim_lo": 10**2,
+        "ylim_lo": 10**-5,
+        "ylim_up": 10**2,
+        "xscale": "log",
+        "yscale": "log",
+        "ystar_valid": 0,
+        "ystar_test": 0,
+   }
 }
 
 list_of_colors = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d']
