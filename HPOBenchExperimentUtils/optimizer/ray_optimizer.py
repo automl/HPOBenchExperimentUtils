6efc953093796f5f1933bb424f083329d4f04991
PhMueller <muller-phil@gmx.net>
2021-07-13 11:26:04 +0200

    Add some more ray optimizer

--- HPOBenchExperimentUtils/optimizer/ray_optimizer.py
+++ HPOBenchExperimentUtils/optimizer/ray_optimizer.py
@@ -5,11 +5,12 @@ from ConfigSpace import ConfigurationSpace
 from ray.tune.utils.log import Verbosity
 from ray import tune as tune
 from ray.tune.schedulers import AsyncHyperBandScheduler
-from ray.tune.suggest import ConcurrencyLimiter
+from ray.tune.suggest import ConcurrencyLimiter, BasicVariantGenerator
 import ray
 
 from HPOBenchExperimentUtils.optimizer.base_optimizer import SingleFidelityOptimizer
 from HPOBenchExperimentUtils.core.bookkeeper import Bookkeeper
+import ConfigSpace.hyperparameters as CSH
 
 from typing import Union, Dict, Any
 from pathlib import Path
@@ -19,6 +20,172 @@ from functools import partial
 
 
 class RayBaseOptimizer(SingleFidelityOptimizer):
+    def __init__(self,
+                 benchmark: Bookkeeper,
+                 search_algorithm: Any,
+                 settings: Dict,
+                 output_dir: Path,
+                 rng: Union[int, None] = 0):
+
+        super(RayBaseOptimizer, self).__init__(benchmark, settings, output_dir, rng)
+
+        # We need to cast the ConfigSpace.configuration space to a ray configuration space
+        self.cs_ray = configspace_to_ray_cs(benchmark.get_configuration_space(rng))
+
+        if not isinstance(search_algorithm, BasicVariantGenerator):
+            search_algorithm = ConcurrencyLimiter(search_algorithm, max_concurrent=1)
+
+        self.search_algorithm = search_algorithm
+
+        self.run_id = f'Ray_optimization_{search_algorithm}_seed_{rng}'
+
+        self.valid_budgets = None
+        self.scheduler = None
+
+    def setup(self):
+        pass
+
+    def run(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _setup_ray():
+        ray.init(local_mode=True,
+                 log_to_driver=False,
+                 include_dashboard=False)
+
+    @staticmethod
+    def _training_function(config, benchmark, main_fidelity_name, valid_budgets, configspace: ConfigurationSpace):
+
+        from ray.tune import get_trial_id
+        id = get_trial_id()
+
+        print(id)
+        print(f'IS ID NONE:{id is None}')
+
+        run_id = SingleFidelityOptimizer._id_generator()
+        config = fix_config_data_types(config, configspace)
+        for budget in valid_budgets:
+            result_dict = benchmark.objective_function(configuration=config,
+                                                       configuration_id=run_id,
+                                                       fidelity={main_fidelity_name: budget})
+            tune.report(function_value=result_dict['function_value'], fidelity=budget)
+
+
+
+class RayBOHBOptimizer(RayBaseOptimizer):
+    def __init__(self,
+                 benchmark: Bookkeeper,
+                 settings: Dict,
+                 output_dir: Path,
+                 rng: Union[int, None] = 0):
+
+        from ray.tune.suggest.bohb import TuneBOHB
+        search_algorithm = TuneBOHB(max_concurrent=1)
+        super(RayBOHBOptimizer, self).__init__(benchmark, search_algorithm, settings, output_dir, rng)
+
+    def run(self):
+        self._setup_ray()
+
+        scheduler = AsyncHyperBandScheduler(max_t=self.max_budget,
+                                            grace_period=self.min_budget,
+                                            reduction_factor=self.settings['reduction_factor'],
+                                            time_attr='fidelity',
+                                            brackets=1)
+
+        self.valid_budgets = [self.max_budget] + [budget for (budget, _) in scheduler._brackets[0]._rungs]
+        self.valid_budgets = list(set(self.valid_budgets))
+        self.valid_budgets.sort()
+
+        from ray.tune.schedulers.hb_bohb import HyperBandForBOHB
+        self.scheduler = HyperBandForBOHB(max_t=self.max_budget,
+                                          reduction_factor=self.settings['reduction_factor'],
+                                          time_attr='fidelity')
+
+        tmp_dir = os.environ.get('TMPDIR', '/tmp/')
+
+        tune.run(partial(self._training_function,
+                         benchmark=self.benchmark,
+                         main_fidelity_name=self.main_fidelity.name,
+                         valid_budgets=self.valid_budgets,
+                         configspace=self.cs),
+                 metric='function_value',
+                 mode='min',
+
+                 # stop=lambda trial_id, results: False,
+                 verbose=Verbosity.V1_EXPERIMENT,
+                 config=self.cs_ray,
+                 search_alg=self.search_algorithm,
+                 scheduler=self.scheduler,   #  ray.tune.schedulers.FIFOScheduler(),  # FiFO is default scheduler
+                 resources_per_trial={"cpu": 1, "gpu": 0},
+                 local_dir=tmp_dir,
+                 # Set this to a very large number, so that this process runs is not bounded by the number of samples.
+                 num_samples=100000000000)
+
+
+class RayOptimizerWithoutFidelity(RayBaseOptimizer):
+    def __init__(self,
+                 benchmark: Bookkeeper,
+                 search_algorithm: Any,
+                 settings: Dict,
+                 output_dir: Path,
+                 rng: Union[int, None] = 0):
+
+        super(RayOptimizerWithoutFidelity, self).__init__(benchmark, search_algorithm, settings, output_dir, rng)
+
+    def run(self):
+        self._setup_ray()
+
+        # For random-search, evaluate only the highest fidelity.
+        self.valid_budgets = [self.max_budget]
+
+        tmp_dir = os.environ.get('TMPDIR', '/tmp/')
+
+        tune.run(partial(self._training_function,
+                         benchmark=self.benchmark,
+                         main_fidelity_name=self.main_fidelity.name,
+                         valid_budgets=self.valid_budgets,
+                         configspace=self.cs),
+                 metric='function_value',
+                 mode='min',
+
+                 # stop=lambda trial_id, results: False,
+                 verbose=Verbosity.V1_EXPERIMENT,
+                 config=self.cs_ray,
+                 search_alg=self.search_algorithm,
+                 # scheduler=ray.tune.schedulers.FIFOScheduler(),  # FiFO is default scheduler
+                 resources_per_trial={"cpu": 1, "gpu": 0},
+                 local_dir=tmp_dir,
+                 # Set this to a very large number, so that this process runs is not bounded by the number of samples.
+                 num_samples=100000000000)
+
+
+class RayRandomSearchOptimizer(RayOptimizerWithoutFidelity):
+    def __init__(self,
+                 benchmark: Bookkeeper,
+                 settings: Dict,
+                 output_dir: Path,
+                 rng: Union[int, None] = 0):
+
+        from ray.tune.suggest.basic_variant import BasicVariantGenerator
+        search_algorithm = BasicVariantGenerator(max_concurrent=1)
+        super(RayRandomSearchOptimizer, self).__init__(benchmark, search_algorithm, settings, output_dir, rng)
+
+
+class RayHyperoptWithoutFidelityOptimizer(RayOptimizerWithoutFidelity):
+    def __init__(self,
+                 benchmark: Bookkeeper,
+                 settings: Dict,
+                 output_dir: Path,
+                 rng: Union[int, None] = 0):
+
+        from ray.tune.suggest.hyperopt import HyperOptSearch
+        search_algorithm = HyperOptSearch()
+        super(RayHyperoptWithoutFidelityOptimizer, self).\
+            __init__(benchmark, search_algorithm, settings, output_dir, rng)
+
+
+class RayHBBaseOptimizer(RayBaseOptimizer):
     """
     A base class for ray optimizer. Ray implements several optimizer, e.g. the hyperopt optimizer.
     We use Hyperband as scheduler.
@@ -42,76 +209,32 @@ class RayBaseOptimizer(SingleFidelityOptimizer):
     (os.environ['TUNE_DISABLE_AUTO_CALLBACK_LOGGERS'] = 1)
     """
 
-    def __init__(self, benchmark: Bookkeeper,
+    def __init__(self,
+                 benchmark: Bookkeeper,
                  search_algorithm: Any,
-                 settings: Dict, output_dir: Path, rng: Union[int, None] = 0):
-        super().__init__(benchmark, settings, output_dir, rng)
+                 settings: Dict,
+                 output_dir: Path,
+                 rng: Union[int, None] = 0):
+        super(RayHBBaseOptimizer, self).__init__(benchmark, search_algorithm, settings, output_dir, rng)
 
-        # We need to cast the ConfigSpace.configuration space to a ray configuration space
-        self.cs_ray = configspace_to_ray_cs(self.benchmark.get_configuration_space(rng))
+    def run(self):
+        """ Execute the optimization run. Return the path where the results are stored. """
+        self._setup_ray()
 
-        self.run_id = f'Ray_optimization_{search_algorithm}_seed_{self.rng}'
-
-        # Limit the number of parallel runs
-        search_algorithm = ConcurrencyLimiter(search_algorithm, max_concurrent=1)
-        self.search_algorithm = search_algorithm
+        # We use here a pure succesive halving. No Hyperband
         self.scheduler = AsyncHyperBandScheduler(max_t=self.max_budget,
                                                  grace_period=self.min_budget,
-                                                 reduction_factor=settings['reduction_factor'],
+                                                 reduction_factor=self.settings['reduction_factor'],
                                                  time_attr='fidelity',
-                                                 # metric='function_value',
-                                                 # mode='min',
-                                                 brackets=1,
-                                                 )
+                                                 brackets=1)
+
         self.valid_budgets = [self.max_budget] + [budget for (budget, _) in self.scheduler._brackets[0]._rungs]
         self.valid_budgets = list(set(self.valid_budgets))
         self.valid_budgets.sort()
 
-    @staticmethod
-    def __training_function(config, benchmark, main_fidelity_name, valid_budgets, configspace: ConfigurationSpace):
-
-        from HPOBenchExperimentUtils.optimizer.base_optimizer import SingleFidelityOptimizer
-        from ConfigSpace.hyperparameters import IntegerHyperparameter, FloatHyperparameter, CategoricalHyperparameter, \
-            OrdinalHyperparameter
-
-        run_id = SingleFidelityOptimizer._id_generator()
-
-        # Cast the configuration into the correct form
-        new_config = {}
-        for hp_name, value in config.items():
-            hp = configspace.get_hyperparameter(hp_name)
-            new_value = None
-            if isinstance(hp, (IntegerHyperparameter, OrdinalHyperparameter)):
-                new_value = int(value)
-            elif isinstance(hp, FloatHyperparameter):
-                new_value = float(value)
-            elif isinstance(hp, CategoricalHyperparameter):
-                hp_type = type(hp.default_value)
-                new_value = hp_type(value)
-            else:
-                print(f'Unknown type of hyperparameter. type{value}')
-
-            new_config[hp_name] = new_value or value
-        config = new_config
-
-        for budget in valid_budgets:
-            result_dict = benchmark.objective_function(configuration=config,
-                                                       configuration_id=run_id,
-                                                       fidelity={main_fidelity_name: budget})
-            tune.report(function_value=result_dict['function_value'], fidelity=budget)
-
-    def setup(self):
-        pass
-
-    def run(self):
-        """ Execute the optimization run. Return the path where the results are stored. """
-        ray.init(local_mode=True,
-                 log_to_driver=False,
-                 include_dashboard=False)
-
         tmp_dir = os.environ.get('TMPDIR', '/tmp/')
 
-        tune.run(partial(self.__training_function,
+        tune.run(partial(self._training_function,
                          benchmark=self.benchmark,
                          main_fidelity_name=self.main_fidelity.name,
                          valid_budgets=self.valid_budgets,
@@ -119,8 +242,6 @@ class RayBaseOptimizer(SingleFidelityOptimizer):
                          ),
                  metric='function_value',
                  mode='min',
-
-                 # stop=lambda trial_id, results: False,
                  verbose=Verbosity.V1_EXPERIMENT,
                  config=self.cs_ray,
                  search_alg=self.search_algorithm,
@@ -132,43 +253,44 @@ class RayBaseOptimizer(SingleFidelityOptimizer):
                  )
 
 
-class RayHyperoptOptimizer(RayBaseOptimizer):
+class RayHBHyperoptOptimizer(RayHBBaseOptimizer):
     def __init__(self, benchmark: Bookkeeper,
                  settings: Dict, output_dir: Path, rng: Union[int, None] = 0):
         from ray.tune.suggest.hyperopt import HyperOptSearch
 
         search_algorithm = HyperOptSearch()
 
-        super(RayHyperoptOptimizer, self).__init__(benchmark=benchmark,
-                                                   search_algorithm=search_algorithm,
-                                                   settings=settings,
-                                                   output_dir=output_dir, rng=rng)
+        super(RayHBHyperoptOptimizer, self).__init__(benchmark=benchmark,
+                                                     search_algorithm=search_algorithm,
+                                                     settings=settings,
+                                                     output_dir=output_dir, rng=rng)
 
 
-class RayBayesOptOptimizer(RayBaseOptimizer):
+class RayHBBayesOptOptimizer(RayHBBaseOptimizer):
     def __init__(self, benchmark: Bookkeeper,
                  settings: Dict, output_dir: Path, rng: Union[int, None] = 0):
         from ray.tune.suggest.bayesopt import BayesOptSearch
 
         search_algorithm = BayesOptSearch()
 
-        super(RayBayesOptOptimizer, self).__init__(benchmark=benchmark,
-                                                   search_algorithm=search_algorithm,
-                                                   settings=settings,
-                                                   output_dir=output_dir, rng=rng)
+        super(RayHBBayesOptOptimizer, self).__init__(benchmark=benchmark,
+                                                     search_algorithm=search_algorithm,
+                                                     settings=settings,
+                                                     output_dir=output_dir, rng=rng)
 
 
-class RayOptunaOptimizer(RayBaseOptimizer):
+class RayHBOptunaOptimizer(RayHBBaseOptimizer):
     def __init__(self, benchmark: Bookkeeper,
                  settings: Dict, output_dir: Path, rng: Union[int, None] = 0):
         from ray.tune.suggest.optuna import OptunaSearch
 
         search_algorithm = OptunaSearch()
 
-        super(RayOptunaOptimizer, self).__init__(benchmark=benchmark,
-                                                 search_algorithm=search_algorithm,
-                                                 settings=settings,
-                                                 output_dir=output_dir, rng=rng)
+        super(RayHBOptunaOptimizer, self).__init__(benchmark=benchmark,
+                                                   search_algorithm=search_algorithm,
+                                                   settings=settings,
+                                                   output_dir=output_dir, rng=rng)
+
 
 
 def configspace_to_ray_cs(cs: ConfigurationSpace):
@@ -200,12 +322,37 @@ def configspace_to_ray_cs(cs: ConfigurationSpace):
     return ray_cs
 
 
-if __name__ == '__main__':
-    cs = ConfigurationSpace()
-    cs.add_hyperparameters([
-        CS.UniformFloatHyperparameter('eta', lower=2 ** -10, upper=1., default_value=0.3, log=True),
-        CS.UniformIntegerHyperparameter('max_depth', lower=1, upper=15, default_value=6, log=False),
-        CS.CategoricalHyperparameter('test', choices=[1,2,3])
-    ])
+def fix_config_data_types(configuration: Dict, configuration_space: CS.ConfigurationSpace) -> Dict:
+    # Cast the configuration into the correct form
+    new_config = {}
+    for hp_name, value in configuration.items():
+        hp = configuration_space.get_hyperparameter(hp_name)
+        new_value = None
+        if isinstance(hp, (CSH.IntegerHyperparameter, CSH.OrdinalHyperparameter)):
+            new_value = int(value)
+        elif isinstance(hp, CSH.FloatHyperparameter):
+            new_value = float(value)
+        elif isinstance(hp, CSH.CategoricalHyperparameter):
+            hp_type = type(hp.default_value)
+            new_value = hp_type(value)
+        else:
+            print(f'Unknown type of hyperparameter. type{value}')
 
-    configspace_to_ray_cs(cs)
+        new_config[hp_name] = new_value or value
+    config = new_config
+    return config
+
+
+__all__ = [RayHyperoptWithoutFidelityOptimizer, RayRandomSearchOptimizer, RayBOHBOptimizer,
+           RayHBOptunaOptimizer, RayHBBayesOptOptimizer, RayHBHyperoptOptimizer]
+
+#
+# if __name__ == '__main__':
+#     cs = ConfigurationSpace()
+#     cs.add_hyperparameters([
+#         CS.UniformFloatHyperparameter('eta', lower=2 ** -10, upper=1., default_value=0.3, log=True),
+#         CS.UniformIntegerHyperparameter('max_depth', lower=1, upper=15, default_value=6, log=True),
+#         CS.CategoricalHyperparameter('test', choices=[1,2,3])
+#     ])
+#
+#     configspace_to_ray_cs(cs)
--- HPOBenchExperimentUtils/utils/optimizer_utils.py
+++ HPOBenchExperimentUtils/utils/optimizer_utils.py
@@ -10,63 +10,6 @@ from HPOBenchExperimentUtils.utils.runner_utils import get_optimizer_settings_na
 _log = logging.getLogger(__name__)
 
 
-def get_sh_ta_runs(min_budget: Union[int, float], max_budget: Union[int, float], eta: int, n0: Optional[int] = None) \
-        -> int:
-    """ Returns total number of configurations for a given SH configuration """
-    sh_iters = int(np.round((np.log(max_budget) - np.log(min_budget)) / np.log(eta), 8))
-    if not n0:
-        n0 = int(eta ** sh_iters)
-    n_configs = n0 * np.power(eta, -np.linspace(0, sh_iters, sh_iters + 1))
-    return int(sum(n_configs))
-
-
-def get_number_ta_runs(iterations: int, min_budget: Union[int, float], max_budget: Union[int, float], eta: int) -> int:
-    """ Returns the total number of configurations (ta runs) for a given HB configuration """
-    s_max = int(np.floor(np.log(max_budget / min_budget) / np.log(eta)))
-
-    all_s = list(range(s_max+1))[::-1]
-    hb_iters = [all_s[i % (s_max + 1)] for i in range(iterations)]
-
-    ta_runs = 0
-    for s in hb_iters:
-        n0 = int(np.floor((s_max + 1) / (s + 1)) * eta ** s)
-        hb_min = eta ** -s * max_budget
-        ta_runs += get_sh_ta_runs(hb_min, max_budget, eta, n0)
-    return int(ta_runs)
-
-
-def is_jsonable(value: Any) -> bool:
-    """ Helperfunction to check if a value is json serializable."""
-    try:
-        json.dumps(value)
-        return True
-    except (TypeError, OverflowError):
-        return False
-
-
-def prepare_dict_for_sending(benchmark_settings: Dict):
-    """
-    Removes all non json-serializable parameter from a dictionary. A warning will be shown if a parameter is removed.
-    Parameters
-    ----------
-    benchmark_settings : Dict
-        Dict container parameters for running a benchmark. E.g. The seed and the output directory.
-        However it may include things in a non-serializable data type.
-    Returns
-    -------
-    Dict
-    """
-    benchmark_dict_for_sending = {}
-    for key, value in benchmark_settings.items():
-        if not is_jsonable(value):
-            if key == 'output_dir':
-                continue
-            _log.warning(f'Value of {key} is not json-serializable. Type was: {type(value)}')
-        else:
-            benchmark_dict_for_sending[key] = value
-    return benchmark_dict_for_sending
-
-
 class OptimizerEnum(Enum):
     """ Enumeration type for the supported optimizers """
     def __str__(self):
@@ -89,6 +32,9 @@ class OptimizerEnum(Enum):
     RAY_HYPEROPT_HB = 'ray_hyperopt_hb'
     RAY_BAYESOPT_HB = 'ray_bayesopt_hb'
     RAY_OPTUNA_HB = 'ray_optuna_hb'
+    RAY_HYPEROPT_NO_FIDELITY = 'ray_hyperopt_no_fidelity'
+    RAY_RANDOMSEARCH = 'ray_randomsearch'
+    RAY_BOHB = 'ray_bohb'
 
 
 def optimizer_str_to_enum(optimizer: Union[OptimizerEnum, str]) -> OptimizerEnum:
@@ -155,12 +101,16 @@ def optimizer_str_to_enum(optimizer: Union[OptimizerEnum, str]) -> OptimizerEnum
 
         elif optimizer == 'ray_hyperopt_hb':
             return OptimizerEnum.RAY_HYPEROPT_HB
-
         elif optimizer == 'ray_bayesopt_hb':
             return OptimizerEnum.RAY_BAYESOPT_HB
-
         elif optimizer == 'ray_optuna_hb':
             return OptimizerEnum.RAY_OPTUNA_HB
+        elif optimizer == 'ray_hyperopt_no_fidelity':
+            return OptimizerEnum.RAY_HYPEROPT_NO_FIDELITY
+        elif optimizer == 'ray_randomsearch':
+            return OptimizerEnum.RAY_RANDOMSEARCH
+        elif optimizer == 'ray_bohb':
+            return OptimizerEnum.RAY_BOHB
 
         else:
             fail = True
@@ -217,14 +167,25 @@ def get_optimizer(optimizer_enum):
         from HPOBenchExperimentUtils.optimizer.randomsearch_optimizer import RandomSearchOptimizer
         optimizer = RandomSearchOptimizer
     elif optimizer_enum is OptimizerEnum.RAY_HYPEROPT_HB:
-        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayHyperoptOptimizer
-        optimizer = RayHyperoptOptimizer
+        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayHBHyperoptOptimizer
+        optimizer = RayHBHyperoptOptimizer
     elif optimizer_enum is OptimizerEnum.RAY_BAYESOPT_HB:
-        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayBayesOptOptimizer
-        optimizer = RayBayesOptOptimizer
+        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayHBBayesOptOptimizer
+        optimizer = RayHBBayesOptOptimizer
     elif optimizer_enum is OptimizerEnum.RAY_OPTUNA_HB:
-        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayOptunaOptimizer
-        optimizer = RayOptunaOptimizer
+        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayHBOptunaOptimizer
+        optimizer = RayHBOptunaOptimizer
+
+    elif optimizer_enum is OptimizerEnum.RAY_HYPEROPT_NO_FIDELITY:
+        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayHyperoptWithoutFidelityOptimizer
+        optimizer = RayHyperoptWithoutFidelityOptimizer
+    elif optimizer_enum is OptimizerEnum.RAY_RANDOMSEARCH:
+        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayRandomSearchOptimizer
+        optimizer = RayRandomSearchOptimizer
+    elif optimizer_enum is OptimizerEnum.RAY_BOHB:
+        from HPOBenchExperimentUtils.optimizer.ray_optimizer import RayBOHBOptimizer
+        optimizer = RayBOHBOptimizer
+
     else:
         raise ValueError(f'Unknown optimizer: {optimizer_enum}')
     return optimizer
@@ -241,3 +202,60 @@ def get_main_fidelity(fidelity_space, settings):
     else:
         fidelity = fidelity_space.get_hyperparameters()[0]
     return fidelity
+
+
+def get_sh_ta_runs(min_budget: Union[int, float], max_budget: Union[int, float], eta: int, n0: Optional[int] = None) \
+        -> int:
+    """ Returns total number of configurations for a given SH configuration """
+    sh_iters = int(np.round((np.log(max_budget) - np.log(min_budget)) / np.log(eta), 8))
+    if not n0:
+        n0 = int(eta ** sh_iters)
+    n_configs = n0 * np.power(eta, -np.linspace(0, sh_iters, sh_iters + 1))
+    return int(sum(n_configs))
+
+
+def get_number_ta_runs(iterations: int, min_budget: Union[int, float], max_budget: Union[int, float], eta: int) -> int:
+    """ Returns the total number of configurations (ta runs) for a given HB configuration """
+    s_max = int(np.floor(np.log(max_budget / min_budget) / np.log(eta)))
+
+    all_s = list(range(s_max+1))[::-1]
+    hb_iters = [all_s[i % (s_max + 1)] for i in range(iterations)]
+
+    ta_runs = 0
+    for s in hb_iters:
+        n0 = int(np.floor((s_max + 1) / (s + 1)) * eta ** s)
+        hb_min = eta ** -s * max_budget
+        ta_runs += get_sh_ta_runs(hb_min, max_budget, eta, n0)
+    return int(ta_runs)
+
+
+def is_jsonable(value: Any) -> bool:
+    """ Helperfunction to check if a value is json serializable."""
+    try:
+        json.dumps(value)
+        return True
+    except (TypeError, OverflowError):
+        return False
+
+
+def prepare_dict_for_sending(benchmark_settings: Dict):
+    """
+    Removes all non json-serializable parameter from a dictionary. A warning will be shown if a parameter is removed.
+    Parameters
+    ----------
+    benchmark_settings : Dict
+        Dict container parameters for running a benchmark. E.g. The seed and the output directory.
+        However it may include things in a non-serializable data type.
+    Returns
+    -------
+    Dict
+    """
+    benchmark_dict_for_sending = {}
+    for key, value in benchmark_settings.items():
+        if not is_jsonable(value):
+            if key == 'output_dir':
+                continue
+            _log.warning(f'Value of {key} is not json-serializable. Type was: {type(value)}')
+        else:
+            benchmark_dict_for_sending[key] = value
+    return benchmark_dict_for_sending
