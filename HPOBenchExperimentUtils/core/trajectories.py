44d0a1f6b805cb787e0790db2557786336cd52aa
PhMueller <muller-phil@gmx.net>
2021-07-21 15:53:36 +0200

    Raise an exception if we cant create a trajectory unambiguously

--- HPOBenchExperimentUtils/core/trajectories.py
+++ HPOBenchExperimentUtils/core/trajectories.py
@@ -1,8 +1,13 @@
 from typing import List
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def create_trajectory(runhistory: List, bigger_is_better: bool):
+
+def create_trajectory(runhistory: List, bigger_is_better: bool, main_fidelity: str = None):
+
     trajectory = []
 
     inc_value = np.inf
@@ -14,7 +19,12 @@ def create_trajectory(runhistory: List, bigger_is_better: bool):
             trajectory.append(record)
             continue
 
-        fidelity = list(record['fidelity'].values())[0]
+        if len(record['fidelity']) == 0:
+            inc_budget = fidelity = 0
+        elif len(record['fidelity']) > 1 and main_fidelity is None:
+            raise ValueError('We found multiple fidelities, but no main fidelity is specified.')
+        else:
+            fidelity = list(record['fidelity'].values())[0]
 
         if ((bigger_is_better
                 and ((abs(fidelity - inc_budget) <= 1e-8 and ((inc_value - record['function_value']) > 1e-8))
